120d061b807b643a93358a7920795f6d54f79d14
Skip <skip@usecurling.com>
2026-04-21 02:51:55 +0000

    feat: Add official form code to publisher record printout

--- src/components/publishers/PrintableS21T.tsx
+++ src/components/publishers/PrintableS21T.tsx
@@ -224,8 +224,8 @@ export function PrintableS21T({ publisher, reports, serviceYear }: Props) {
           </tbody>
         </table>
 
-        <div className="mt-auto pt-8 text-[10px] text-gray-800 font-bold tracking-tight text-right">
-          S-21-T 11/23
+        <div className="mt-auto pt-8 flex justify-end w-full">
+          <span className="text-[12px] text-black font-bold tracking-tight">S-21-T 11/23</span>
         </div>
       </div>
     </div>
